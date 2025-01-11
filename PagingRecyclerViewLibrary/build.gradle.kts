912019952f36b95abb91b5de77b026a95ad2ad0c
Mohammad Hosein <m.hosein.developer@gmail.com>
2025-01-11 11:19:41 +0330

    setup gradle 1

--- PagingRecyclerViewLibrary/build.gradle.kts
+++ PagingRecyclerViewLibrary/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 21
 
         group = "com.github.M-Hosein-Developer"
-        version = "1.0.0"
+        version = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
@@ -39,8 +39,8 @@ android {
                     from(components["release"])
 
                     groupId = "com.github.M-Hosein-Developer"
-                    artifactId = "CustomEmptyState"
-                    version = "1.0.0"
+                    artifactId = "PagingRecyclerViewLib"
+                    version = "1.0.1"
                 }
             }
 
